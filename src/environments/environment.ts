53c5deffe6619a1b217df52c983bee4267d71800
Raghav Maheshwari <raghav.maheshwari@nagarro.com>
2022-11-09 23:58:18 +0530

    ask for client id and tenant id to me

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   clientId: '506ebcbc-3ac8-4ee7-bcae-7939e9b04266',
-  authorityId: '8d2588db-69d1-4f2b-a8bd-99dfd68491d0'
+  authorityId: '8d2588db-69d1-4f2b-a8bd-99dfd68491d0',
+  something:''
 };
 
 /*
